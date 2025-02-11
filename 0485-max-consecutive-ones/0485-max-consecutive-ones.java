7b362c706a3056cecd1b448d487ff78a9e97d0cf
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-02-12 01:30:28 +0530

    Time: 3 ms (52.34%), Space: 50.5 MB (24.04%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -7,11 +7,12 @@ class Solution {
                 count++;
             }
             else{
-                max=Math.max(max,count);
+                
                 count=0;
             }
+            max=Math.max(max,count);
         }
-        max=Math.max(max,count);
+        max=Math.max(max,count);max=Math.max(max,count);
         return max;
     }
 }
