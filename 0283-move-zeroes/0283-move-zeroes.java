1e0f45fc4bfba91179eea0c9edabd880b486ef7e
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-02-12 01:04:06 +0530

    Time: 3 ms (23%), Space: 46.2 MB (18.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -11,14 +11,7 @@ class Solution {
             i++;
         }
         else if(nums[i]==0 && nums[j]==0){
-            if(nums[j]==0){
-                j++;
-            }
-            else{
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-            }
+           j++;
         } 
         else{
             i++;
