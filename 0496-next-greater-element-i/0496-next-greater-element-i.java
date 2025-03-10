6610e4fe722e95b4cba1b0c756cbc381c5cabf37
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-03-10 13:08:24 +0530

    Time: 5 ms (18.77%), Space: 43.3 MB (81.9%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -5,14 +5,26 @@ class Solution {
         int result[]=new int[n2];
         int k=0;
         Stack<Integer> s = new Stack<>();
-        for(int i=n2-1;i>=0;i--){
-            while(!s.isEmpty() && nums2[i]>=s.peek()){
-                s.pop();
-            }
-            result[i]=s.isEmpty()?-1:s.peek();
-            s.push(nums2[i]);
+        // for(int i=n2-1;i>=0;i--){
+        //     while(!s.isEmpty() && nums2[i]>=s.peek()){
+        //         s.pop();
+        //     }
+        //     result[i]=s.isEmpty()?-1:s.peek();
+        //     s.push(nums2[i]);
+        // }
+
+        for(int i=0;i<n2;i++){
+           while(!s.isEmpty() && nums2[i]>nums2[s.peek()] ){
+            result[s.peek()]=nums2[i];
+            s.pop();
+           }
+
+           s.push(i);
         }
-          int[] ans = new int[n1];
+        while(s.size()!=0){
+            result[s.pop()]=-1;
+        }
+        int[] ans = new int[n1];
         for (int i = 0; i < n1; i++) {
             ans[i] = -1; // Default to -1
             for (int j = 0; j < n2; j++) {
