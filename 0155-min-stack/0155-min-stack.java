69f068b50cc29c471e501c2cb793fa981c80d265
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-03-12 01:50:47 +0530

    Time: 4 ms (97.18%), Space: 44.8 MB (67.63%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,38 +1,105 @@
+// class MinStack {
+
+//     Stack<Integer> s = new Stack<>();
+//     Stack<Integer> min = new Stack<>();
+
+//     public MinStack() {
+        
+//     }
+    
+//     public void push(int val) {
+//         if(s.size()==0){
+//             min.push(val);
+//         }
+//         else{
+//             if(min.peek()>val){
+//                 min.push(val);
+//             }
+//             else{
+//                 min.push(min.peek());
+//             }
+//         }
+//         s.push(val);
+//     }
+    
+//     public void pop() {
+//         s.pop();
+//         min.pop();
+//     }
+    
+//     public int top() {
+//        return s.peek();
+//     }
+    
+//     public int getMin() {
+//         return min.peek();
+//     }
+// }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
 class MinStack {
 
-    Stack<Integer> s = new Stack<>();
-    Stack<Integer> min = new Stack<>();
+    Stack<Long> s = new Stack<>();
+    long min=-1;
 
     public MinStack() {
         
     }
     
     public void push(int val) {
+        long x = (long) val;
         if(s.size()==0){
-            min.push(val);
+            s.push(x);
+            min=x;
         }
         else{
-            if(min.peek()>val){
-                min.push(val);
+            if(min>x){
+                s.push(2*x-min);
+                min=x;
             }
             else{
-                min.push(min.peek());
+                s.push(x);
             }
         }
-        s.push(val);
     }
     
     public void pop() {
+       if(s.size()==0){
+        return;
+       }
+       else{
+        if(s.peek()<min){
+            long old=(2*min)-s.peek();
+            min=old;
+        }
         s.pop();
-        min.pop();
+       }
     }
     
     public int top() {
-       return s.peek();
+        long q=s.peek();
+        if(s.size()==0){
+            return -1;
+        }
+        if(q<min){
+            return (int) min;
+        }
+        return (int) q;
     }
     
     public int getMin() {
-        return min.peek();
+        if(s.size()==0){
+            return -1;
+        }
+        return (int) min;
     }
 }
 
