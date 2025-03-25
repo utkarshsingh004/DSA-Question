11d054b2e0aba6b3c5d72727db6c5d6adc152519
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-03-25 21:39:17 +0530

    Time: 1 ms (99.57%), Space: 57 MB (70%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,12 +4,11 @@ class Solution {
         int sum=0;
         int max=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-            sum+=nums[i];
-             max=Math.max(max,sum);
-            if(sum<0){
-                sum=0;
-            }
-           
+           sum+=nums[i];
+           max=Math.max(max,sum);
+           if(sum<0){
+            sum=0;
+           }
         }
         return max;
     }
