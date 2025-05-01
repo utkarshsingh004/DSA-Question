3b5e2ab548d32443bf01a1b62f384db6d71ed9f0
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-05-02 02:16:26 +0530

    Time: 18 ms (16.59%), Space: 44.9 MB (26.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -7,8 +7,9 @@ class Solution {
             return false;
         }
         HashMap <Character, Integer> h = new HashMap<>();
-        HashMap <Character, Integer> h2 = new HashMap<>();
-        // for(int i=0;i<m;i++){
+        // HashMap <Character, Integer> h2 = new HashMap<>();
+        
+        //  for(int i=0;i<m;i++){
         //     char ch = s.charAt(i);
         //     if(!h.containsKey(ch)){
         //         h.put(ch,temp);
@@ -21,9 +22,7 @@ class Solution {
         // }
         // for(int i=0;i<n;i++){
         //     char ch = t.charAt(i);
-        //     if(!h.containsKey(ch)){
-        //        return false;
-        //     }
+           
         //     if(!h2.containsKey(ch)){
         //         h2.put(ch,temp);
         //     }
@@ -31,17 +30,11 @@ class Solution {
         //         Integer res=h2.get(ch);
         //         res++;
         //         h2.put(ch,res);
-        //         if(h.get(ch)<h2.get(ch)){
-        //             return false;
-        //         }
         //     }
         // }
-        // for(var key: h.keySet()){
-        //     if(h.get(key)%2!=0){
-        //         return false;
-        //     }
-        // }
-         for(int i=0;i<m;i++){
+        // return h.equals(h2);
+
+        for(int i=0;i<m;i++){
             char ch = s.charAt(i);
             if(!h.containsKey(ch)){
                 h.put(ch,temp);
@@ -54,16 +47,20 @@ class Solution {
         }
         for(int i=0;i<n;i++){
             char ch = t.charAt(i);
-           
-            if(!h2.containsKey(ch)){
-                h2.put(ch,temp);
+            if(!h.containsKey(ch)){
+                return false;
             }
             else{
-                Integer res=h2.get(ch);
-                res++;
-                h2.put(ch,res);
+                Integer res=h.get(ch);
+                res--;
+                h.put(ch,res);
             }
         }
-        return h.equals(h2);
+         for(var key: h.keySet()){
+            if(h.get(key)!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
