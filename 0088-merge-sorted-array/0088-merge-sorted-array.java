79144e52dbf5fafabbf99617b95bd27287dcbc0d
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-05-18 12:50:39 +0530

    Time: 0 ms (100%), Space: 42.4 MB (42.22%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -3,16 +3,15 @@ class Solution {
         int k=m+n-1;
         int i=m-1;
         int j=n-1;
-        while(i>=0&&j>=0){
+        while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
-                nums1[k--]=nums1[i--];
+                nums1[k--]=nums1[i];
+                i--;
             }
             else{
-                nums1[k--]=nums2[j--];
+                nums1[k--]=nums2[j];
+                j--;
             }
-        }
-        while(i>=0){
-            nums1[k--]=nums1[i--];
         }
         while(j>=0){
             nums1[k--]=nums2[j--];
