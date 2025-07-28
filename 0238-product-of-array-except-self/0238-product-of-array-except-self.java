03d5524313c65ece3f9a967ea5a70eb4d281ced2
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-07-29 04:35:06 +0530

    Time: 3 ms (19.63%), Space: 55.7 MB (35.98%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,19 +1,53 @@
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int n=nums.length;
+//         int prefix=1;
+//         int suffix=1;
+//         int ans [] = new int[n];
+//         ans[0]=1;
+//         for(int i=1;i<n;i++){
+//            prefix*=nums[i-1];
+//            ans[i]=prefix;
+//         }
+//         for(int i=n-2;i>=0;i--){
+            
+//             suffix*=nums[i+1];
+//             ans[i]*=suffix;
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n=nums.length;
-        int prefix=1;
-        int suffix=1;
-        int ans [] = new int[n];
-        ans[0]=1;
+        int pref[]=new int[n];
+        int suff[]=new int[n];
+        pref[0]=nums[0];
         for(int i=1;i<n;i++){
-           prefix*=nums[i-1];
-           ans[i]=prefix;
+            pref[i]=nums[i]*pref[i-1];
         }
+        suff[n-1]=nums[n-1];
         for(int i=n-2;i>=0;i--){
-            
-            suffix*=nums[i+1];
-            ans[i]*=suffix;
+            suff[i]=nums[i]*suff[i+1];
         }
-        return ans;
+        for(int i=0;i<n;i++){
+            if(i==0){
+                nums[i]=suff[i+1];
+            }
+            else if(i==n-1){
+                nums[i]=pref[i-1];
+            }
+            else{
+                nums[i]=pref[i-1]*suff[i+1];
+            }
+        }
+        // int mul=1;
+        // for(int i=0;i<n;i++){
+        //     mul*=nums[i];
+        // }
+        // for(int i=0;i<n;i++){
+        //     nums[i]=mul^i;
+        // }
+        return nums;
     }
 }
