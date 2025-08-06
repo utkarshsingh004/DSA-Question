919495fc31cd18d8216ae8e61747c949325613a0
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-08-07 05:07:39 +0530

    Time: 0 ms (100%), Space: 83.9 MB (49.64%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
@@ -1,35 +1,42 @@
 class Solution {
-    public int[] findPeakGrid(int[][] mat) {
-        int rows = mat.length;
-        int cols = mat[0].length;
 
-        int left = 0;
-        int right = cols - 1;
-
-        while (left <= right) {
-            int midCol = left + (right - left) / 2;
-            int maxRow = 0;
-
-            // Find row with max element in midCol
-            for (int i = 0; i < rows; i++) {
-                if (mat[i][midCol] > mat[maxRow][midCol]) {
-                    maxRow = i;
-                }
+    private int max(int arr[][], int mid) {
+        int n = arr.length;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[max][mid] < arr[i][mid]) {
+                max = i;
             }
+        }
+        return max;
+    }
 
-            boolean leftIsBigger = midCol - 1 >= 0 && mat[maxRow][midCol - 1] > mat[maxRow][midCol];
-            boolean rightIsBigger = midCol + 1 < cols && mat[maxRow][midCol + 1] > mat[maxRow][midCol];
+    public int[] findPeakGrid(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int[] arr = new int[2];  // use 'arr' since that's what you used
+        int i = 0;
+        int j = m - 1;
 
-            if (!leftIsBigger && !rightIsBigger) {
-                // Found peak
-                return new int[]{maxRow, midCol};
-            } else if (leftIsBigger) {
-                right = midCol - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            int maxElement = max(mat, mid);
+
+            // Safe boundary checks
+            boolean leftCheck = (mid == 0) || (mat[maxElement][mid] > mat[maxElement][mid - 1]);
+            boolean rightCheck = (mid == m - 1) || (mat[maxElement][mid] > mat[maxElement][mid + 1]);
+
+            if (leftCheck && rightCheck) {
+                arr[0] = maxElement;
+                arr[1] = mid;
+                return arr;
+            } else if (mid < m - 1 && mat[maxElement][mid] < mat[maxElement][mid + 1]) {
+                i = mid + 1;
             } else {
-                left = midCol + 1;
+                j = mid - 1;
             }
         }
 
-        return new int[]{-1, -1};
+        return new int[]{-1, -1};  // fallback, though problem guarantees a peak
     }
 }
