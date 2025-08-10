7d4ccad4a6f7d6f44ce8537fe6a2e60952a182c4
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-08-10 21:22:28 +0530

    Time: 13 ms (14.97%), Space: 47 MB (93.68%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,46 +1,56 @@
 class Solution {
 
-    private int max(int[] arr) {
-        int m = 0;
-        for (int val : arr) {
-            m = Math.max(m, val);
-        }
-        return m;
-    }
-
-    private int sum(int[] arr) {
-        int m = 0;
-        for (int val : arr) {
-            m += val;
-        }
-        return m;
-    }
-
-    private int total(int[] arr, int capacity) {
-        int days = 1;
-        int sum = 0;
-        for (int weight : arr) {
-            if (sum + weight > capacity) {
-                days++;
-                sum = 0;
+    private int max(int arr[]){
+        int m=0;
+        for(int i=0;i<arr.length;i++){
+            if(m<arr[i]){
+                m=arr[i];
             }
-            sum += weight;
         }
-        return days;
+        return m;
+    }
+
+    private int sum(int arr[]){
+         int m=0;
+        for(int i=0;i<arr.length;i++){
+            m+=arr[i];
+        }
+        return m;
+    }
+
+    private int total(int arr[], int num){
+        int n=arr.length;
+        int count=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            if(sum==num){
+                count++;
+                sum=0;
+            }
+            else if(sum>num){
+                count++;
+                sum=arr[i];
+            }
+        }
+        if(sum!=0){
+            count++;
+        }
+        return count;
     }
 
     public int shipWithinDays(int[] weights, int days) {
-        int i = max(weights);
-        int j = sum(weights);
-        int ans = j;
-
-        while (i <= j) {
-            int mid = i + (j - i) / 2;
-            if (total(weights, mid) <= days) {
-                ans = mid;
-                j = mid - 1;
-            } else {
-                i = mid + 1;
+        int i=max(weights);
+        int j=sum(weights);
+        int ans=0;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(total(weights, mid)<=days){
+                ans=mid;
+                j=mid-1;
+            }
+            else{
+                i=mid+1;
             }
         }
         return ans;
