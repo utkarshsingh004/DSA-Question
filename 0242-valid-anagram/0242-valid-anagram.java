dbc43569ba9f380fede0ed78277da5a47afa92bb
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-08-12 04:06:39 +0530

    Time: 16 ms (21.89%), Space: 44.9 MB (16.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,12 +3,29 @@ class Solution {
         int n=s.length();
         int m=t.length();
         if(n!=m) return false;
-        char a[]=s.toCharArray();
-        char b[]=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        for(int i=0;i<s.length();i++){
-            if(a[i]!=b[i]) return false;
+        HashMap<Character, Integer> h = new HashMap<>();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(!h.containsKey(ch)){
+                h.put(ch,1);
+            }
+            else{
+                h.put(ch,h.get(ch)+1);
+            }
+        }
+        for(int i=0;i<m;i++){
+            char ch=t.charAt(i);
+            if(!h.containsKey(ch)){
+                return false;
+            }
+            else{
+                h.put(ch,h.get(ch)-1);
+            }
+        }
+        for(char c: h.keySet()){
+            if(h.get(c)!=0){
+                return false;
+            }
         }
         return true;
     }
