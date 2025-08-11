fef5213d76e83d3e6e32fa91ab5803bcb67057a2
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-08-12 04:00:39 +0530

    Time: 3 ms (92.03%), Space: 44.8 MB (34.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,65 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int m = s.length();
-        int n = t.length();
-        Integer temp=1;
-        if(m!=n){
-            return false;
-        }
-        HashMap <Character, Integer> h = new HashMap<>();
-        // HashMap <Character, Integer> h2 = new HashMap<>();
-        
-        //  for(int i=0;i<m;i++){
-        //     char ch = s.charAt(i);
-        //     if(!h.containsKey(ch)){
-        //         h.put(ch,temp);
-        //     }
-        //     else{
-        //         Integer res=h.get(ch);
-        //         res++;
-        //         h.put(ch,res);
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     char ch = t.charAt(i);
-           
-        //     if(!h2.containsKey(ch)){
-        //         h2.put(ch,temp);
-        //     }
-        //     else{
-        //         Integer res=h2.get(ch);
-        //         res++;
-        //         h2.put(ch,res);
-        //     }
-        // }
-        // return h.equals(h2);
-
-        for(int i=0;i<m;i++){
-            char ch = s.charAt(i);
-            if(!h.containsKey(ch)){
-                h.put(ch,temp);
-            }
-            else{
-                Integer res=h.get(ch);
-                res++;
-                h.put(ch,res);
-            }
-        }
-        for(int i=0;i<n;i++){
-            char ch = t.charAt(i);
-            if(!h.containsKey(ch)){
-                return false;
-            }
-            else{
-                Integer res=h.get(ch);
-                res--;
-                h.put(ch,res);
-            }
-        }
-         for(var key: h.keySet()){
-            if(h.get(key)!=0){
-                return false;
-            }
+        int n=s.length();
+        int m=t.length();
+        if(n!=m) return false;
+        char a[]=s.toCharArray();
+        char b[]=t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int i=0;i<s.length();i++){
+            if(a[i]!=b[i]) return false;
         }
         return true;
     }
