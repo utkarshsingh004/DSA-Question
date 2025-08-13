4da8d25213ddb41dbdd315146a38529fa73b672b
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-08-14 04:52:12 +0530

    Time: 0 ms (100%), Space: 42.9 MB (10.94%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,25 +10,15 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        Stack<Integer>s = new Stack<>();
-        ListNode temp=head;
-        while(temp!=null){
-            s.push(temp.val);
-            temp=temp.next;
+        ListNode current=head;
+        ListNode prev=null;
+        ListNode next=null;
+        while(current!=null){
+            next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
         }
-        ListNode t = null;
-        ListNode t2=null;
-        while(s.size()!=0){
-            int a=s.pop();
-            ListNode ptr = new ListNode(a);
-            if(t==null){
-                t=ptr;
-            }
-            else{
-                t2.next=ptr;
-            }
-            t2=ptr;
-        }
-    return t;
+        return prev;
     }
 }
