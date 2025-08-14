3f05b1c3c06333b71be732d06907ed4cd29094e1
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-08-15 05:18:30 +0530

    Time: 0 ms (100%), Space: 41.1 MB (97.12%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,39 +1,26 @@
 class MyStack {
 
     private Queue<Integer> q1 = new LinkedList<>();
-    private Queue<Integer> q2 = new LinkedList<>();
     
     public void push(int x) {
-        q1.add(x);
+        if(q1.size()==0){
+            q1.add(x);
+        }
+        else{
+            q1.add(x);
+            for(int i=0;i<q1.size()-1;i++){
+                q1.add(q1.remove());
+            }
+        }
     }
     
     public int pop() {
-        if(empty()){
-            return -1;
-        }
-        while(q1.size()!=1){
-            q2.add(q1.remove());
-        }
-        int peek=q1.remove();
-        while(q2.size()!=0){
-            q1.add(q2.remove());
-        }
-        return peek;
+       int peek=q1.remove();
+       return peek;
     }
     
     public int top() {
-        if(empty()){
-            return -1;
-        }
-        while(q1.size()!=1){
-            q2.add(q1.remove());
-        }
-        int peek=q1.remove();
-        q2.add(peek);
-        while(q2.size()!=0){
-            q1.add(q2.remove());
-        }
-        return peek;
+        return q1.peek();
     }
     
     public boolean empty() {
