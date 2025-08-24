e7e11224d1d0c32587343d0684458184c71365ca
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-08-24 18:09:30 +0530

    Time: 1 ms (71.93%), Space: 45.6 MB (67.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,36 +1,31 @@
 class Solution {
-
-    private void zero(int[][] arr, int i, int j){
-        for(int l=0;l<arr.length;l++){
-            // arr[i][l]=Integer.MIN_VALUE;
-            if(arr[l][j]!=0){
-              arr[l][j]=Integer.MAX_VALUE-10;
-            }  
-           
-        }
-        for(int l=0;l<arr[i].length;l++){
-            if(arr[i][l]!=0){
-                arr[i][l]=Integer.MAX_VALUE-10;
-            }
-            // arr[l][j]=Integer.MIN_VALUE;
-        }
-    }
-
-
     public void setZeroes(int[][] matrix) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int a=Integer.MIN_VALUE+10;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    zero(matrix, i, j);
+                    matrix[i][j]=-1;
+                    for(int k=0;k<m;k++){
+                        if(matrix[i][k]!=0){
+                            matrix[i][k]=a;
+                        }
+                    }
+                    for(int k=0;k<n;k++){
+                        if(matrix[k][j]!=0){
+                            matrix[k][j]=a;
+                        }
+                    }
                 }
             }
         }
-       for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==Integer.MAX_VALUE-10){
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==a){
                     matrix[i][j]=0;
                 }
             }
-        }
+         }
     }
 }
