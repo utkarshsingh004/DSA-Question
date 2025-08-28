c422954d16998f456d318dd40022acf6a27bc45e
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-08-29 01:36:31 +0530

    Time: 0 ms (100%), Space: 41.1 MB (41.35%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,32 +1,8 @@
 class Solution {
-
-    private int pow(int num){
-        int p=1;
-        for(int i=1;i<=num;i++){
-           p*=2;
-        }
-        return p;
-    }
-
     public boolean isPowerOfTwo(int n) {
-        if(n==1 || n==2){
-            return true;
-        }
-        int i=0;
-        int j=31;
-        while(i<=j){
-            int mid=i+(j-i)/2;
-            int ans=pow(mid);
-            if(ans==n){
-                return true;
-            }
-            else if(ans<n){
-                i=mid+1;
-            }
-            else{
-                j=mid-1;
-            }
-        }
+        if(n<=0) return false;
+        int a=n & (n-1);
+        if(a==0)return true;
         return false;
     }
 }
