dbf3bb1ea35b42b48ff40791cececfe5e7849fd7
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-09-03 00:26:53 +0530

    Time: 21 ms (45.15%), Space: 45.2 MB (46.07%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,56 +1,35 @@
 class Solution {
+    private long time(int arr[], int mid){
+        long sum=0;
+        for(int i=0;i<arr.length;i++){
+           sum += (long) Math.ceil((double) arr[i] / mid);
 
-   private int total(int arr[], int s){
-     int count=0;
-     for(int i=0;i<arr.length;i++){
-        count+=Math.ceil((double) arr[i]/s);
-     }
-     return count;
-   }
-
+        }
+        return sum;
+    }
     public int minEatingSpeed(int[] piles, int h) {
         int n=piles.length;
-        int max=Integer.MIN_VALUE;
+        int max=0;
         for(int i=0;i<n;i++){
-          if(max<piles[i]){
-            max=piles[i];
-          }
+            if(max<piles[i]){
+                max=piles[i];
+            }
         }
         int i=1;
         int j=max;
+        int ans=0;
         while(i<=j){
             int mid=i+(j-i)/2;
-            int ans=total(piles,mid);
-            if(ans<=h){
-                j=mid-1;;
+            long a=time(piles,mid);
+            if(a<=(long)h){
+                ans=mid;
+                j=mid-1;
             }
             else{
                 i=mid+1;
             }
+            
         }
-      return i;
+        return ans;
     }
 }
-
-
-// class Solution {
-//     public int minEatingSpeed(int[] piles, int h) {
-//         int n=piles.length;
-//         int max=0;
-//         for(int i=0;i<n;i++){
-//             if(max<piles[i]){
-//                 max=piles[i];
-//             }
-//         }
-//         for(int i=1;i<=max;i++){
-//             int count=0;
-//             for(int j=0;j<n;j++){
-//                 count += Math.ceil((double)(piles[j]) / i);
-//             }
-//             if(count<=h){
-//                 return i;
-//             }
-//         }
-//         return -1;
-//     }
-// }
