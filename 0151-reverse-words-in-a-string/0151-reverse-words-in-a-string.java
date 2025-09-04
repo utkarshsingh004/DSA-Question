ee12c98fbc8937df599ce469bb5303f403575cb6
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-09-04 05:31:08 +0530

    Time: 6 ms (85.55%), Space: 44.4 MB (23.95%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,59 +1,13 @@
-// class Solution {
-
-//    private void swap(String[] str, int i, int j){
-//    while(i<j){
-//       String temp=str[i];
-//       str[i]=str[j];
-//       str[j]=temp;
-//       i++;
-//       j--;
-//     }
-//    }
-
-//     public String reverseWords(String s) {
-//         String ans="";
-//         s=s.trim();
-//         String[] str = s.split(" ");
-//         int n=str.length-1;
-//         swap(str,0,n);
-//         for(int i=0;i<=n;i++){
-//             ans+=str[i];
-//             if(ans.charAt(i)!=' ' && i!=n){
-//                  ans+=" ";
-//             }
-//         }
-//         return ans;
-        
-//     }
-// }
-
 class Solution {
     public String reverseWords(String s) {
-        s=s.trim();
-        int n=s.length();
-        Stack<String> st = new Stack<>();
-        String str ="";
-        for(int i=0;i<n;i++){
-            if(s.charAt(i)==' '){
-                st.push(str);
-                str="";
-            }
-            else{
-                str+=s.charAt(i);
-            }
+       StringBuilder sb = new StringBuilder();
+       String st=s.trim();
+       String arr[] = st.split(" ");
+       for(int i=arr.length-1;i>=0;i--){
+        if(arr[i]!=""){
+            sb.append(arr[i]+" ");
         }
-        st.push(str);
-        String result="";
-        while(st.size()!=1){
-            if(st.peek()==""){
-                st.pop();
-            }
-           else{
-            result+=st.pop();
-            result+=" ";
-           }
-        }
-        result+=st.pop();
-        return result;
+       }
+       return sb.toString().trim();
     }
 }
