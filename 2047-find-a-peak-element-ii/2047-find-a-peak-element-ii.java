876278d7d12a9b462c73b242358c33676444d9cf
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-09-08 06:38:12 +0530

    Time: 0 ms (100%), Space: 84 MB (50.51%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
@@ -1,42 +1,46 @@
 class Solution {
-
-    private int max(int arr[][], int mid) {
-        int n = arr.length;
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[max][mid] < arr[i][mid]) {
-                max = i;
-            }
+    private int maximum(int arr[][],int m){
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[max][m]<arr[i][m]) max=i;
         }
         return max;
     }
-
     public int[] findPeakGrid(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int[] arr = new int[2];  // use 'arr' since that's what you used
-        int i = 0;
-        int j = m - 1;
-
-        while (i <= j) {
-            int mid = i + (j - i) / 2;
-            int maxElement = max(mat, mid);
-
-            // Safe boundary checks
-            boolean leftCheck = (mid == 0) || (mat[maxElement][mid] > mat[maxElement][mid - 1]);
-            boolean rightCheck = (mid == m - 1) || (mat[maxElement][mid] > mat[maxElement][mid + 1]);
-
-            if (leftCheck && rightCheck) {
-                arr[0] = maxElement;
-                arr[1] = mid;
-                return arr;
-            } else if (mid < m - 1 && mat[maxElement][mid] < mat[maxElement][mid + 1]) {
-                i = mid + 1;
-            } else {
-                j = mid - 1;
-            }
+        int arr[] = new int[2];
+        int n=mat.length;
+        int m=mat[0].length;
+        if(n==1 && m==1){
+            return new int[]{0,0};
+        }
+        int zeroIdx=maximum(mat,0);
+        if(mat[zeroIdx][0]>mat[zeroIdx][1]){
+            arr[0]=zeroIdx;
+            arr[1]=0;
+            return arr;
+        }
+        int lastIdx=maximum(mat,m-1);
+        if(mat[lastIdx][m-1]>mat[lastIdx][m-2]){
+            arr[0]=lastIdx;
+            arr[1]=m-1;
+            return arr;
         }
 
-        return new int[]{-1, -1};
+        int i=1;
+        int j=m-2;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            int idx=maximum(mat,mid);
+            if(mat[idx][mid]>mat[idx][mid-1] && mat[idx][mid]>mat[idx][mid+1]){
+                arr[0]=idx;
+                arr[1]=mid;
+                return arr;
+            }
+            else if(mat[idx][mid]<mat[idx][mid-1]){
+                j=mid-1;
+            }
+            else if(mat[idx][mid]<mat[idx][mid+1]) i=mid+1;
+        }
+        return new int[]{-1,-1};
     }
 }
