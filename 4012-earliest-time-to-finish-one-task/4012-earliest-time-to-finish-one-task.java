bc6e8a8cbd9c3de3d174c5f3c3ee295c4d068088
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-09-17 14:26:58 +0530

    Time: 1 ms (76.01%), Space: 44.8 MB (91.87%) - LeetHub

--- 4012-earliest-time-to-finish-one-task/4012-earliest-time-to-finish-one-task.java
+++ 4012-earliest-time-to-finish-one-task/4012-earliest-time-to-finish-one-task.java
@@ -1,9 +1,13 @@
 class Solution {
     public int earliestTime(int[][] tasks) {
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<tasks.length;i++){
-            min=Math.min(min,tasks[i][0]+tasks[i][1]);
+        int result = Integer.MAX_VALUE;
+        for(int i=0;i<tasks.length;i++)
+        {
+            if(tasks[i][0]+tasks[i][1] < result)
+            {
+                result = tasks[i][0]+tasks[i][1];
+            }
         }
-        return min;
+        return result;
     }
 }
