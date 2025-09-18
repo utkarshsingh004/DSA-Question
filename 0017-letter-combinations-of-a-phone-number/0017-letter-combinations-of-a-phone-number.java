84d709d15717562ce244412fa632a69a64df8d3e
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-09-18 08:34:39 +0530

    Time: 5 ms (27.79%), Space: 42.4 MB (26.77%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,24 +1,29 @@
 class Solution {
+    private void helper(String digits, HashMap<Character,String> h, int idx, String ans, List<String> l){
+        if(idx==digits.length()){
+            if(ans.length()==0) return;
+            l.add(ans);
+            return;
+        }
+        char currentDigit=digits.charAt(idx);
+        String letter=h.get(currentDigit);
+        for(int i=0;i<letter.length();i++){
+            helper(digits,h,idx+1,ans+letter.charAt(i),l);
+        }
+    }
     public List<String> letterCombinations(String digits) {
-    List<String> ans = new ArrayList<>();
-    if (digits == null || digits.length() == 0) return ans;
-
-    String[] kp = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
-    backtrack(digits, 0, "", kp, ans);
-    return ans;
-}
-
-private void backtrack(String digits, int index, String res, String[] kp, List<String> ans) {
-    if (index == digits.length()) {
-        ans.add(res);
-        return;
+        HashMap<Character,String> h=new HashMap<>();
+        h.put('2',"abc");
+        h.put('3',"def");
+        h.put('4',"ghi");
+        h.put('5',"jkl");
+        h.put('6',"mno");
+        h.put('7',"pqrs");
+        h.put('8',"tuv");
+        h.put('9',"wxyz");
+        
+        List<String> l=new ArrayList<>();
+        helper(digits,h,0,"",l);
+        return l;
     }
-
-    String choices = kp[digits.charAt(index) - '0'];
-    for (int i = 0; i < choices.length(); i++) {
-        backtrack(digits, index + 1, res + choices.charAt(i), kp, ans);
-    }
-}
-
 }
