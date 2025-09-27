5d310c3bc2840a99838c7508401e40bcb6a1ef79
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-09-27 14:57:12 +0530

    Time: 5 ms (91.95%), Space: 41.8 MB (30.85%) - LeetHub

--- 0830-largest-triangle-area/0830-largest-triangle-area.java
+++ 0830-largest-triangle-area/0830-largest-triangle-area.java
@@ -1,26 +1,20 @@
 class Solution {
-    private double area(double s, double a, double b, double c){
-       double val = s * (s - a) * (s - b) * (s - c);
-       if (val <= 0) return 0;
-       return Math.sqrt(val);
+    private double area(int[] p1, int[] p2, int[] p3) {
+        return 0.5 * Math.abs(
+            p1[0] * (p2[1] - p3[1]) +
+            p2[0] * (p3[1] - p1[1]) +
+            p3[0] * (p1[1] - p2[1])
+        );
     }
-    private double distance(int x1, int x2, int y1, int y2){
-        int x=x2-x1;
-        int y=y2-y1;
-        return Math.sqrt((Math.pow(x,2))+Math.pow(y,2));
-    }
+
     public double largestTriangleArea(int[][] points) {
-        int n=points.length;
-        double max=0;
-        for(int i=0;i<n-2;i++){
-            for(int j=i+1;j<n-1;j++){
-                for(int k=j+1;k<n;k++){
-                    double a=distance(points[i][0],points[j][0],points[i][1],points[j][1]);
-                    double b=distance(points[i][0],points[k][0],points[i][1],points[k][1]);
-                    double c=distance(points[k][0],points[j][0],points[k][1],points[j][1]);
-                    double s=(a+b+c)/2;
-                    double ar=area(s,a,b,c);
-                    max=Math.max(max,ar);
+        int n = points.length;
+        double max = 0;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    double ar = area(points[i], points[j], points[k]);
+                    max = Math.max(max, ar);
                 }
             }
         }
