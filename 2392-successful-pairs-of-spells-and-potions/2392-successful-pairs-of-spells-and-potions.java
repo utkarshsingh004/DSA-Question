8c49bd1b138ee65a19f8224e3f8158d50827c02c
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2025-10-08 13:12:23 +0530

    Time: 816 ms (5.17%), Space: 67.4 MB (6.88%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -1,35 +1,26 @@
-import java.util.*;
-
 class Solution {
-    public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        int n = spells.length;
-        int m = potions.length;
-
-        // store spell with its original index
-        int[][] arr = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            arr[i][0] = spells[i]; // spell value
-            arr[i][1] = i;         // original index
-        }
-
-        // sort spells and potions
-        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
-        Arrays.sort(potions);
-
-        int[] ans = new int[n];
-        int j = m - 1;
-
-        // iterate spells in ascending order
-        for (int i = 0; i < n; i++) {
-            int spell = arr[i][0];
-            // move potion pointer from right to left until product is large enough
-            while (j >= 0 && (long)spell * potions[j] >= success) {
-                j--;
+    private int helper(int p[], int e, long s){
+        int i=0;
+        int j=p.length-1;
+        int a=p.length;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if((long)p[mid]*e>=s){
+                a=mid;
+                j=mid-1;
             }
-            // all potions after j are successful
-            ans[arr[i][1]] = m - (j + 1);
+            else i=mid+1;
         }
-
-        return ans;
+        return a;
+    }
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int m=potions.length;
+        Arrays.sort(potions);
+        for(int i=0;i<spells.length;i++){
+            int idx=helper(potions,spells[i],success);
+            System.out.print(idx);
+            spells[i]=m-idx;
+        }
+       return spells;
     }
 }
