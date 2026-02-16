24a00e96d7ba089f420e0585a36c357572f609d4
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2026-02-17 03:29:11 +0530

    Time: 0 ms (100%), Space: 42.4 MB (15.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,30 +1,15 @@
 class Solution {
-    public int climbStairs(int n) {
-        if(n==1 || n==2) return n;
-        int dp[]=new int[n];
-        dp[0]=1;
-        dp[1]=2;
-        for(int i=2;i<n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+    private int helper(int i, int n, int dp[]){
+        if(i>n) return 0;
+        if(i==n){
+        return 1;
         }
-        return dp[n-1];
+        if(dp[i]!=-1) return dp[i];
+        return dp[i]=helper(i+1, n, dp) + helper(i+2, n, dp);
+    }
+    public int climbStairs(int n) {
+        int dp[] = new int [n+1];
+        Arrays.fill(dp,-1);
+        return helper(0,n, dp);
     }
 }
-
-// class Solution {
-//     private int climb(int i, int n, int dp[]){
-//         if(i==n){
-//             return 1;
-//         }
-//         if(i>n) return 0;
-//         if(dp[i]!=-1) return dp[i];
-//         int step=climb(i+1,n,dp);
-//         int step2=climb(i+2,n,dp);
-//         return dp[i]=step+step2;
-//     }
-//     public int climbStairs(int n) {
-//         int dp[]=new int[n+1];
-//         Arrays.fill(dp,-1);
-//         return climb(0,n,dp);
-//     }
-// }
