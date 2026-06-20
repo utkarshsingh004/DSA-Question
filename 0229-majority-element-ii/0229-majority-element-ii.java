bbbc7208d5f73c66805fedd615538a48f880909a
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2026-06-21 01:22:28 +0530

    Time: 110 ms (6.07%), Space: 52.6 MB (52.18%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,14 +3,34 @@ class Solution {
         int n=nums.length;
         List<Integer> arr = new ArrayList<>();
         int k=n/3;
-        HashMap<Integer,Integer> h = new HashMap<>();
+        int el1=nums[0];
+        int el2=Integer.MIN_VALUE;
+        int c1=0;
+        int c2=0;
         for(int i=0;i<n;i++){
-            if(!h.containsKey(nums[i])) h.put(nums[i],1);
-            else h.put(nums[i],h.get(nums[i])+1);
+            if(nums[i]==el1) c1++;
+            else if(nums[i]==el2) c2++;
+            else if(c1==0){
+                el1=nums[i];
+                c1++;
+            }
+            else if(c2==0){
+               el2=nums[i];
+               c2++;
+            } 
+            else if(nums[i]!=el1 && nums[i]!=el2){
+                c1--;
+                c2--;
+            }
         }
-        for(int i:h.keySet()){
-            if(h.get(i)>k) arr.add(i);
+        c1=0;
+        c2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==el1) c1++;
+            if(nums[i]==el2) c2++;
         }
+        if(c1>k) arr.add(el1);
+        if(c2>k) arr.add(el2);
         return arr;
     }
 }
