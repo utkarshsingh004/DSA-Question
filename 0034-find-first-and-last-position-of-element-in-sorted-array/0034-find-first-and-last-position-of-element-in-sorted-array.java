40f10f52f1eb4bf7b86328a2493528f52c4f9c79
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2026-06-22 09:26:16 +0530

    Time: 0 ms (100%), Space: 47.9 MB (85.94%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,51 +1,37 @@
 class Solution {
-
-   private int first(int[] nums, int target){
-     int n = nums.length;
-     int i=0;
-     int j=n-1;
-     int f=-1;
-     while(i<=j){
-            int mid=i+(j-i)/2;
+    private int fO(int nums[], int target){
+        int i=0;
+        int j=nums.length-1;
+        int idx=-1;
+        while(i<=j){
+            int mid=(i+(j-i)/2);
             if(nums[mid]==target){
-                f=mid;
+                idx=mid;
                 j=mid-1;
             }
-            else if(nums[mid]<target){
-                i=mid+1;
-            }
-            else{
-                j=mid-1;
-            }
+            else if(nums[mid]>target) j=mid-1;
+            else i=mid+1;
         }
-        return f;
-   }
-
-   private int last(int[] nums, int target){
-     int n = nums.length;
-     int i=0;
-     int j=n-1;
-     int l=-1;
-     while(i<=j){
-            int mid=i+(j-i)/2;
+        return idx;
+    }
+    private int lO(int nums[], int target){
+        int i=0;
+        int j=nums.length-1;
+        int idx=-1;
+        while(i<=j){
+            int mid=(i+(j-i)/2);
             if(nums[mid]==target){
-                l=mid;
+                idx=mid;
                 i=mid+1;
             }
-            else if(nums[mid]<target){
-                i=mid+1;
-            }
-            else{
-                j=mid-1;
-            }
+            else if(nums[mid]<target) i=mid+1;
+            else j=mid-1;
         }
-        return l;
-   }
-
+        return idx;
+    }
     public int[] searchRange(int[] nums, int target) {
-        int arr[] = new int[2];
-        arr[0]=first(nums, target);
-        arr[1]=last(nums, target);
-        return arr;
+       int a=fO(nums,target);
+       int b=lO(nums,target);
+       return new int[]{a,b};
     }
 }
