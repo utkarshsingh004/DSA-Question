5f0562134887868ebea58076c544fa56b3181cf6
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2026-06-24 20:38:33 +0530

    Time: 8 ms (68.65%), Space: 48.2 MB (19.61%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,6 +1,6 @@
 class Solution {
     private long helper(int arr[], int m){
-        int count=0;
+        long count=0;
         for(int i=0;i<arr.length;i++){
            count += (long) (arr[i] + m - 1) / m;
         }
@@ -17,14 +17,11 @@ class Solution {
         while(min<=max){
             int mid=min+(max-min)/2;
             long a=helper(piles,mid);
-            if(a==(long)h){
+            if(a<=(long)h){
                 idx=mid;
                 max=mid-1;
             }
-            else if(a>h){
-                min=mid+1;
-            }
-            else max=mid-1;
+            else min=mid+1;
         }
         return idx;
     }
