e91fc54b569d1311b18d8d61da4d48d1b528fc70
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2026-06-28 01:07:40 +0530

    Time: 0 ms (100%), Space: 42.6 MB (87.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,17 @@
 class Solution {
-    private int maxLoot(int nums[], int idx, int dp[]){
-        if(idx>=nums.length) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        int steal=nums[idx]+maxLoot(nums,idx+2,dp);
-        int skip=maxLoot(nums,idx+1,dp);
-        return dp[idx]=Math.max(steal,skip);
-    }
+    // private int helper(int nums[], int idx){
+         
+    // }
     public int rob(int[] nums) {
-        int dp[]=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return maxLoot(nums,0,dp);
+        int n=nums.length;
+        if(n==1) return nums[0];
+        if(n==2) return Math.max(nums[0],nums[1]);
+        int dp[]=new int [n];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
     }
 }
