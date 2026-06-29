f340d5e29de28dc544fbc61312467b7316a32212
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2026-06-29 23:37:15 +0530

    Time: 0 ms (100%), Space: 43.3 MB (80.55%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,20 +1,19 @@
 class Solution {
-    private int count(int[][] mat, int row, int col, int d, int r, int dp[][]){
-        if(d>row || r>col) return 0;
-        if(mat[d][r]==1) return 0;
-        if(d==row && r==col) return 1;
-        if(dp[d][r]!=-1) return dp[d][r];
-        int a=count(mat,row,col,d+1,r,dp);
-        int b=count(mat,row,col,d,r+1,dp);
-        return dp[d][r]=a+b;
+    private int helper(int [][]nums, int n, int m, int dp[][]){
+        if(nums[0][0]==1) return 0;
+        if(n==0 && m==0) return 1;
+        if(n<0 || m<0) return 0;
+        if(nums[n][m]==1) return 0;
+        if(dp[n][m]!=-1) return dp[n][m];
+        return dp[n][m]=helper(nums,n-1,m,dp)+helper(nums,n,m-1,dp);
     }
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int row=obstacleGrid.length;
-        int col=obstacleGrid[0].length;
-        int dp[][]=new int[row][col];
-        for(int i=0;i<row;i++){
+    public int uniquePathsWithObstacles(int[][] o) {
+        int n=o.length;
+        int m=o[0].length;
+        int dp[][]=new int[n][m];
+        for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-        return count(obstacleGrid,row-1,col-1,0,0,dp);
+        return helper(o,n-1,m-1,dp);
     }
 }
