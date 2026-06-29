e07f92f27fd8605c6500762cbf54a6bb660489ec
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2026-06-29 20:31:32 +0530

    Time: 0 ms (100%), Space: 42.4 MB (12.73%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,15 +1,19 @@
 class Solution {
-    private int unique(int m, int n, int dp[][]){
-        if(m==1|| n==1) return 1;
-        if(m<=0 || n<=0) return 0;
+    private int helper(int m, int n, int dp[][]){
+        if(m==0 && n==0){
+            return 1;
+        }
+        if(m<0||n<0) return 0;
         if(dp[m][n]!=-1) return dp[m][n];
-        return dp[m][n]=unique(m-1,n,dp)+unique(m,n-1,dp);
+        int l=helper(m-1,n,dp);
+        int u=helper(m,n-1,dp);
+        return dp[m][n]=l+u;
     }
     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m+1][n+1];
-        for(int i=0;i<m+1;i++){
+        int dp[][]=new int[m+1][n+1];
+        for(int i=0;i<m;i++){
             Arrays.fill(dp[i],-1);
         }
-        return unique(m,n,dp);
+        return helper(m-1,n-1,dp);
     }
 }
