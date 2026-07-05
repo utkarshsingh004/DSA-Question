d11804ebd9da01a920552e31e2aaebead21e2fe2
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2026-07-06 03:06:56 +0530

    Time: 1 ms (100%), Space: 46.4 MB (56.85%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -9,87 +9,26 @@
  * }
  */
 class Solution {
-    private int helper2(ListNode head,int carry){
-        if(head.next==null){
-            int sum=head.val+carry;
-            head.val=sum%10;
-            return sum/10;
-        }
-        int sum=carry+head.val;
-        head.val=sum%10;
-        return helper2(head.next,sum/10);
-    }
-    private int helper(ListNode l1, ListNode l2, int carry){
-        if(l2.next==null){
-            int sum=carry+l1.val+l2.val;
-            System.out.print(sum+" ");
-            l1.val=sum%10;
-            return sum/10;
-        }
-        int sum=carry+l1.val+l2.val;
-        System.out.print(sum+" ");
-        l1.val=sum%10;
-        return helper(l1.next,l2.next,sum/10);
-    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int s1=0;
-        int s2=0;
-        ListNode ptr=l1;
-        while(ptr!=null){
-            s1++;
-            ptr=ptr.next;
-        }
-        ptr=l2;
-        while(ptr!=null){
-            s2++;
-            ptr=ptr.next;
-        }
-        if(s1==s2){
-            System.out.print("First");
-            ptr=l1;
-            while(ptr.next!=null){
-               ptr=ptr.next;
+        ListNode dummy = new ListNode();
+        ListNode temp=dummy;
+        int carry=0;
+        while(l1!=null || l2!=null || carry!=0){
+            int sum=0;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
             }
-            int carry=helper(l1,l2,0);
-            if(carry>0){
-               ListNode temp=new ListNode(carry);
-                ptr.next=temp;
-            } 
-            return l1;
-        }
-        else if(s1>s2){
-            ptr=l1;
-            for(int i=0;i<s2;i++) ptr=ptr.next;
-            int c=helper(l1,l2,0);
-            if(c>0){
-                int c1=helper2(ptr,c);{
-                    if(c1>0){
-                        System.out.print("F");
-                        ptr=l1;
-                        while(ptr.next!=null) ptr=ptr.next;
-                        ListNode temp= new ListNode(c1);
-                        ptr.next=temp;
-                    }
-                }
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
             }
-            return l1;
+            sum+=carry;
+            carry=sum/10;
+            ListNode t= new ListNode(sum%10);
+            temp.next=t;
+            temp=t;
         }
-        else{
-            ptr=l2;
-            for(int i=0;i<s1;i++) ptr=ptr.next;
-            int c=helper(l2,l1,0);
-            if(c>0){
-                int c1=helper2(ptr,c);{
-                    if(c1>0){
-                        System.out.print("S");
-                        ptr=l2;
-                        while(ptr.next!=null) ptr=ptr.next;
-                        ListNode temp= new ListNode(c1);
-                        ptr.next=temp;
-                    }
-                }
-            }
-            return l2;
-        }
+        return dummy.next;
     }
 }
