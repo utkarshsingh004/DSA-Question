6f847381d5ff16f96813d74267175e1b804216ff
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2026-07-11 00:09:24 +0530

    Time: 25 ms (5.67%), Space: 46.6 MB (11.3%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,23 +1,24 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n1=nums1.length;
-        int n2=nums2.length;
-        Stack<Integer> s = new Stack<>();
-        HashMap<Integer, Integer> h = new HashMap<>();
-        for(int i=n2-1;i>=0;i--){
-            while(!s.isEmpty() && nums2[i]>s.peek()){
-                s.pop();
+        int n=nums1.length;
+        int m=nums2.length;
+        HashMap<Integer,Integer> h = new HashMap<>();
+        for(int i=0;i<m;i++){
+            boolean flag=false;
+            for(int j=i;j<m;j++){
+                if(nums2[i]<nums2[j]){
+                    h.put(nums2[i],nums2[j]);
+                    flag=true;
+                    break;
+                }
             }
-            if(s.size()==0){
-                h.put(nums2[i],-1);
-            }
-            else{
-                h.put(nums2[i],s.peek());
-            }
-            s.push(nums2[i]);
+            if(!flag) h.put(nums2[i],-1);
         }
-        for(int i=0;i<n1;i++){
-            nums1[i]=h.get(nums1[i]);
+        for(int i:h.keySet()) System.out.println(i+" "+h.get(i));
+        for(int i=0;i<n;i++){
+            if(h.containsKey(nums1[i])){
+                nums1[i]=h.get(nums1[i]);
+            } 
         }
         return nums1;
     }
