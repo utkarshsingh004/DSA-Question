ce0ba4bb4efd94876a64e435ce21fc257ac26fbc
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2026-07-11 00:16:57 +0530

    Time: 4 ms (74.59%), Space: 45.5 MB (11.3%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -3,18 +3,16 @@ class Solution {
         int n=nums1.length;
         int m=nums2.length;
         HashMap<Integer,Integer> h = new HashMap<>();
-        for(int i=0;i<m;i++){
-            boolean flag=false;
-            for(int j=i;j<m;j++){
-                if(nums2[i]<nums2[j]){
-                    h.put(nums2[i],nums2[j]);
-                    flag=true;
-                    break;
-                }
+        Stack<Integer> s = new Stack<>();
+        for(int i=m-1;i>=0;i--){
+            while(!s.isEmpty() && s.peek()<nums2[i]) s.pop();
+            if(s.isEmpty()){
+                h.put(nums2[i],-1);
             }
-            if(!flag) h.put(nums2[i],-1);
+            else h.put(nums2[i],s.peek());
+            s.push(nums2[i]);
         }
-        for(int i:h.keySet()) System.out.println(i+" "+h.get(i));
+        // for(int i:h.keySet()) System.out.println(i+" "+h.get(i));
         for(int i=0;i<n;i++){
             if(h.containsKey(nums1[i])){
                 nums1[i]=h.get(nums1[i]);
