81f331a9f275f3f265fb8d5339cc70ebc16aa5c9
Utkarsh Kumar Singh <singhutkarshkumar097@gmail.com>
2026-07-11 01:01:54 +0530

    Time: 15 ms (61.25%), Space: 48.9 MB (12.81%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -2,17 +2,29 @@ class Solution {
     public int[] nextGreaterElements(int[] nums2) {
         int m=nums2.length;
         int arr[] = new int[m];
-        for(int i=0;i<m;i++){
-            boolean flag=false;
-            for(int j=1;j<m;j++){
-                int idx=(i+j)%m;
-                if(nums2[i]<nums2[idx]){
-                    arr[i]=nums2[idx];
-                    flag=true;
-                    break;
+        // for(int i=0;i<m;i++){
+        //     boolean flag=false;
+        //     for(int j=1;j<m;j++){
+        //         int idx=(i+j)%m;
+        //         if(nums2[i]<nums2[idx]){
+        //             arr[i]=nums2[idx];
+        //             flag=true;
+        //             break;
+        //         }
+        //     }
+        //     if(!flag) arr[i]=-1;
+        // }
+        Stack<Integer> s = new Stack<>();
+        for(int i=2*m-1;i>=0;i--){
+            int idx=i%m;
+            while(!s.isEmpty() && s.peek()<=nums2[idx]) s.pop();
+            if(i<m){
+                if(s.isEmpty()){
+                    arr[idx]=-1;
                 }
+                else arr[idx]=s.peek();
             }
-            if(!flag) arr[i]=-1;
+            s.push(nums2[idx]);
         }
         return arr;
     }
